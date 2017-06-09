19367abe84d75762d588ea2411d856708b2ec874
prakash <pgk639117@gmail.com>
2017-06-09 21:22:51 +0200

    firdt

---
+++ first-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//import { OtherComponent } from './other';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Inline Template</h1>',
+  //<fa-other></fa-other>',
+  styles: ['h1 { color:red;}']
+})
+export class AppComponent {
+  //title = 'Application works!';
+}
